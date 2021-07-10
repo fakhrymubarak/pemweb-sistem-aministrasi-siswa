50be6823d0737d21c518379535cca676774a0bf9
SyauqiMA <syauqiabrar@gmail.com>
2021-07-10 19:38:24 +0700

    moved configs and registered new blueprint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,21 +6,22 @@ db = SQLAlchemy()
 def create_app():
     # Create app and basic configs here (DB, etc.)
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'my secret key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/sistem_rapor_pweb'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_pyfile('config.py')
     # initialize the database
     
     db.init_app(app)
     
-    # Register blueprints
-    from .routes.siswa import siswa
-    from .routes.jurusan import jurusan
-    from .routes.mapel import mapel
-    from .routes.periode_ajaran import periode_ajaran
-    app.register_blueprint(siswa, url_prefix='/api')
-    app.register_blueprint(jurusan, url_prefix='/api')
-    app.register_blueprint(mapel, url_prefix='/api')
-    app.register_blueprint(periode_ajaran, url_prefix='/api')
+    with app.app_context():
+        # Register blueprints
+        from .routes.siswa import siswa
+        from .routes.jurusan import jurusan
+        from .routes.mapel import mapel
+        from .routes.periode_ajaran import periode_ajaran
+        from .routes.nilai import nilai
+        app.register_blueprint(siswa, url_prefix='/api')
+        app.register_blueprint(jurusan, url_prefix='/api')
+        app.register_blueprint(mapel, url_prefix='/api')
+        app.register_blueprint(periode_ajaran, url_prefix='/api')
+        app.register_blueprint(nilai, url_prefix='/api')
 
     return app
