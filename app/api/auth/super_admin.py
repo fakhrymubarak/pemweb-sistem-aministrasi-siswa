2fcb670fb2ffbea43f3807ad97437ea1a0ed2b17
SyauqiMA <syauqiabrar@gmail.com>
2021-07-13 08:40:27 +0700

    added validation

--- app/api/auth/super_admin.py
+++ app/api/auth/super_admin.py
@@ -14,28 +14,33 @@ def super_admin_register():
         return make_response(jsonify({'error': 'Username diperlukan!'}), 400)
     if not params.get('password'):
         return make_response(jsonify({'error': 'Password diperlukan!'}), 400)
+    # if Username already exists, reject the attempt
+    existing_username = Admin.query.filter_by(username=params.get('username')).first()
+    if existing_username:
+        return make_response(jsonify({'error': 'Username sudah ada!'}), 400)
     # Process the registering
     user = Admin(**params)
     user.hash_password()
     db.session.add(user)
     db.session.commit()
-    return make_response(jsonify({'message': 'OK'}), 200)
+    return make_response(jsonify({'message': 'Register success'}), 200)
 
 @super_admin.route('/admin_login', methods=["POST"])
 def super_admin_login():
     params = request.form
-    user = db.session.query(Admin).filter_by(username=params.get('username')).first()
     # validate the existence of params
     if not params.get('username'):
         return make_response(jsonify({'error': 'Username diperlukan!'}), 400)
     if not params.get('password'):
         return make_response(jsonify({'error': 'Password diperlukan!'}), 400)
-    authorized = user.check_pasword(params.get('password'))
+    # check the login
+    user = Admin.query.filter_by(username=params.get('username')).first()
+    authorized = user.check_password(params.get('password'))
     if not authorized:
         return make_response(jsonify({'error': 'Email or password invalid'}), 401)
     
     # Create access token
     expires = datetime.timedelta(days=2)
     access_token = create_access_token(identity=str(user.id_admin), expires_delta=expires)
-    return make_response(jsonify({'token': access_token}), 200)
+    return make_response(jsonify({'message': 'login success', 'token': access_token}), 200)
     
